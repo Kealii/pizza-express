d1d6ecb1a5bd9dc5d705002314f741e971d7fcb0
Michael Wong <michael.brian.wong@gmail.com>
2016-03-02 14:15:01 -0700

    App responds to a port

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.set('port', process.env.PORT || 4000);
+app.locals.title = 'Pizza Express';
+
+app.get('/', (request, response) => {
+    response.send('Hello World!');
+});
+
+if (!module.parent) {
+    app.listen(app.get('port'), () => {
+        console.log(`${app.locals.title} is running on ${app.get('port')}.`);
+    });
+}
