0e2998e206ce0c54ee673c64884d23da2c259cd0
Michael Wong <michael.brian.wong@gmail.com>
2016-03-02 20:43:41 -0700

    Basic Unit Tests passing

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const app = express();
 
-app.set('port', process.env.PORT || 4000);
+const path = require('path');
+
+app.use(express.static('static'));
+
+app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Pizza Express';
 
 app.get('/', (request, response) => {
-    response.send('Hello World!');
+    response.sendFile(path.join(__dirname, '/static/index.html'));
 });
 
 if (!module.parent) {
     app.listen(app.get('port'), () => {
         console.log(`${app.locals.title} is running on ${app.get('port')}.`);
-    });
+});
 }
+
+module.exports = app;
