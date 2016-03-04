e5f31dd49024001ed52435c500478ca12560a1f8
Michael Wong <michael.brian.wong@gmail.com>
2016-03-04 09:40:25 -0700

    Adds testing for an individual pizza

--- server.js
+++ server.js
@@ -16,6 +16,11 @@ app.get('/', (request, response) => {
     response.render('index');
 });
 
+app.get('/pizzas/:id', (request, response) => {
+    var pizza = app.locals.pizzas[request.params.id];
+    response.render('pizza', { pizza: pizza });
+});
+
 app.post('/pizzas', (request, response) => {
     var id = generateId();
     app.locals.pizzas[id] = request.body
